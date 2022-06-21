0bf75b876a61e77da093bb6fe72cabc4a9dd19e1
AnaSpinetti <30602516+AnaSpinetti@users.noreply.github.com>
2022-06-21 17:01:16 -0300

    global.css atualizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import './globals.css'
+import './global.css'
 import { Header } from './components/header/Header'
+import styles from './app.module.css';
 
 function App() {
 
   return (
     <>
     <Header />
+    <div className={styles.wrapper}>
+      <aside></aside>
+      <main></main>
+    </div>
     </>
   )
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,11 @@
 import styles from './header.module.css'
+import igniteLogo from '../../../images/IgniteSimbol.svg'
 
 export function Header(){
     return (
         <>
             <header className={styles.header}>
-                <img src="../../../images/IgniteSimbol.svg"></img>
+                <img src={igniteLogo} alt='Logotipo do ignite'></img>
                 <h3>Ignite Feed</h3>
             </header>
         </>
